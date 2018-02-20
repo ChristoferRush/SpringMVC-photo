94318fee5516b6d9a4e5be3e3c0753c594c8db6e
ChristoferRush <christoferrush@gmail.com>
2018-02-20 12:40:43 +0100

    loginController

--- src/main/java/pl/fotobudka/controller/LoginController.java
+++ src/main/java/pl/fotobudka/controller/LoginController.java
@@ -2,17 +2,32 @@ package pl.fotobudka.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.fotobudka.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = "/adminpanel", method = RequestMethod.GET)
-    public String loginToAdminPanel(Model model){
+    public String loginToAdminPanelForm(Model model){
         User user = new User();
         model.addAttribute("user", user);
         return "loginPage";
+    }
+
+    @RequestMapping(value = "/adminpanel", method = RequestMethod.POST)
+    public String loginToAdminPanel(HttpSession session,
+                                    @ModelAttribute User user){
+        User userToLogin = user;
+        if (user.getRole().getAccess() == 6){
+            session.setAttribute("admin", userToLogin);
+            return "adminPanelPage";
+        } else {
+            return "loginPage";
+        }
     }
 }
