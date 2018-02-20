61ec1e8c4930fba6c47d1fcc104d44d46a624dae
ChristoferRush <christoferrush@gmail.com>
2018-02-20 11:25:47 +0100

    entities

---
+++ src/main/java/pl/fotobudka/entity/Role.java
@@ -0,0 +1,47 @@
+package pl.fotobudka.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer access;
+
+    @OneToMany
+    private List<User> userList = new ArrayList<User>();
+
+    public Role(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAccess() {
+        return access;
+    }
+
+    public void setAccess(Integer access) {
+        this.access = access;
+    }
+}
